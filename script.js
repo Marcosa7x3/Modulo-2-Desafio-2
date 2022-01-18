d20679979c61bfcd1ad9110b3a42b0cf433a5539
Marcos Adriano <marcosadriano645@gmail.com>
2022-01-17 22:07:58 -0300

    Segundo Desafio do Modulo 2

---
+++ script.js
@@ -0,0 +1,15 @@
+let idade = 25
+if(idade >= 20){
+    console.log("Você é maior de idade")
+}
+
+let humano = true
+if(idade >= 18 && humano == true){
+    console.log("Você é humano e é maior de iadade")
+}
+
+let nome = "Marcos"
+let mes = "Julho"
+if(mes === "Dezembro" || mes === "Julho"){
+    console.log("Você faz aniversário em Julho ou Dezembro")
+}
